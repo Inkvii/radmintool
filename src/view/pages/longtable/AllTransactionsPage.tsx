a880cfb9d090e9cc9a9752faabfb0ba588336cd0
Lubos Verzich <lve@paymorrow.com>
2021-03-23 16:24:32 +0100

    Getting filter data from backend

--- src/view/pages/longtable/AllTransactionsPage.tsx
+++ src/view/pages/longtable/AllTransactionsPage.tsx
@@ -1,10 +1,24 @@
 import {Container, Paper} from "@material-ui/core";
 import Filter from "view/pages/longtable/fragment/Filter"
-import {useState} from "react";
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 export default function AllTransactionsPage() {
 
-    const [headers, setHeaders] = useState(["first", "second", "third"])
+    const [headers, setHeaders] = useState<string[]>([])
+
+    useEffect(() => {
+        axios.get("http://localhost:8080/transactions/longTransactions", {headers: {"Content-Type": "application/json"}})
+            .then(result => {
+                let head: string[] = []
+
+                Object.keys(result.data).forEach(header => {
+                    head.push(header)
+                })
+                setHeaders(head)
+            })
+    }, [])
+
 
     const triggerFilter = (filterHeaders: {}) => {
         console.info("Trigger filtering according to: "+ JSON.stringify(filterHeaders))
@@ -18,11 +32,16 @@ export default function AllTransactionsPage() {
     )
 }
 
+interface Props {
 
-function LongTable() {
+}
+
+function LongTable(props: Props) {
     return (
         <Paper>
             <h2>long table</h2>
+
+
         </Paper>
     )
 }
